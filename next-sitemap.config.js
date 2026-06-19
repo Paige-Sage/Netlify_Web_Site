1761e59749bb33945fc7985038896c72611f9898
Gary <garywa@microsoft.com>
2026-06-18 23:01:59 -0700

    sms opt in page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://paigeandsage.club',
   generateRobotsTxt: true,
-  exclude: ['/pricing', '/careers', '/casting', '/casting/*', '/privacy', '/terms'],
+  exclude: ['/pricing', '/careers', '/casting', '/casting/*', '/privacy', '/terms', '/sms-opt-in'],
 }
